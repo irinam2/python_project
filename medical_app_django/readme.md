08f906fd607b964699fa6e4b3f2d722fda425d84
Irina Muchaeva <muchaevair@gmail.com>
2024-06-03 20:28:50 +0300

    second commit

--- medical_app_django/readme.md
+++ medical_app_django/readme.md
@@ -1,9 +1,11 @@
 Смените настройки подключения к СУБД на свои в medical_app_django/settings.py в переменной DATABASES  
 Откройте командную строку (cmd) или терминал в директории medical_app_django.   
-Создайте виртуальное окружение (conda create --name conda_venv python=3.8).  
+Создайте виртуальное окружение (conda create conda_venv python=3.8).  
 Активируйте виртуальное окружение (conda activate conda_venv).   
 Установите необходимые библиотеки: pip install  -r requirements.txt   
 Выполнить:  
     python manage.py makemigrations services  
-    python manage.py migrate services
-Запустите проект python manage.py runserver  
+    python manage.py migrate services  
+    python manage.py makemigrations services  
+    python manage.py migrate services  
+Запустите проект python manage.py runserver
--- medical_app_django/services/services.py
+++ medical_app_django/services/services.py
@@ -4,7 +4,7 @@ from .models import Service
 
 def search_services(query):
     """
-    Функция для поиска медицинских услуг на основе поискового запроса.
+    Функция для поиска медицинских услуг на основе поискового запроса на сайте "Zoon.ru".
 
     Args:
         query: Поисковый запрос.
@@ -13,20 +13,19 @@ def search_services(query):
         Список объектов `Service` с найденными услугами.
     """
 
-    url = f"https://med.ru/ru/search/node?keys={query}"
+    url = f"https://zoon.ru/search/?city=msk&query={query}"
     response = requests.get(url)
     response.raise_for_status()  # Поднимает исключение при ошибке запроса
     soup = BeautifulSoup(response.content, 'html.parser')
 
     services = []
     # Находим все элементы услуг на странице
-    services_elements = soup.find_all('h3', class_='search-result__title')
-
-    print(services_elements)
+    services_elements = soup.find_all('a', class_='title-link js-item-url')
     for service_element in services_elements:
         # Извлекаем данные из элементов HTML
-        clinic = service_element.find('a').text.strip()
-        link = service_element.find('a')['href']
+        print(service_element)
+        clinic = service_element.text.strip()
+        link = service_element['href']
 
         # Создание объекта Service
         service = Service.objects.create(
